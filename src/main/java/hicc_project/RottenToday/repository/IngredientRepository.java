a93baf475e52a092f3723487e4a604d75a324d22
DESKTOP-100KUGP\준연 <whwnsdus11@naver.com>
2025-07-27 15:07:13 +0900

    feat: JpaRepository를 상속한 레포지토리 인터페이스 생성

--- src/main/java/hicc_project/RottenToday/repository/IngredientRepository.java
+++ src/main/java/hicc_project/RottenToday/repository/IngredientRepository.java
@@ -1,19 +1,10 @@
 package hicc_project.RottenToday.repository;
 
 import hicc_project.RottenToday.entity.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface IngredientRepository {
-
-    Ingredient save(Ingredient ingredient);
-
-    List<Ingredient> findAll();
-
-    Ingredient findById(Long id);
-
-    void update(Ingredient ingredient);
-
-    void delete(Ingredient ingredient);
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 
 }
---
+++ src/main/java/hicc_project/RottenToday/repository/RefrigeratorIngredientRepository.java
@@ -0,0 +1,7 @@
+package hicc_project.RottenToday.repository;
+
+import hicc_project.RottenToday.entity.RefrigeratorIngredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefrigeratorIngredientRepository extends JpaRepository<RefrigeratorIngredient, Long> {
+}
