230db8fc51a0aa6bde1a89880feeb725841411fb
DESKTOP-100KUGP\준연 <whwnsdus11@naver.com>
2025-07-30 00:11:51 +0900

    Revert "오류 수정"
    This reverts commit 294625a3bfd9a35ec9b74127e34e5e94e26ea51c.

--- src/main/java/hicc_project/RottenToday/repository/AllergyRepository.java
+++ src/main/java/hicc_project/RottenToday/repository/AllergyRepository.java
@@ -1,7 +1,6 @@
 package hicc_project.RottenToday.repository;
 
-import hicc_project.RottenToday.entity.Allergy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AllergyRepository extends JpaRepository<Allergy, Long> {
+public interface AllergyRepository extends JpaRepository<AllergyRepository, Long> {
 }
--- src/main/java/hicc_project/RottenToday/repository/CookHistoryRepository.java
+++ src/main/java/hicc_project/RottenToday/repository/CookHistoryRepository.java
@@ -1,7 +1,6 @@
 package hicc_project.RottenToday.repository;
 
-import hicc_project.RottenToday.entity.CookHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CookHistoryRepository extends JpaRepository<CookHistory, Long> {
+public interface CookHistoryRepository extends JpaRepository<CookHistoryRepository, Long> {
 }
