294625a3bfd9a35ec9b74127e34e5e94e26ea51c
magnetogithub <jwlim011@gmail.com>
2025-07-29 22:29:00 +0900

    오류 수정

--- src/main/java/hicc_project/RottenToday/entity/Taste.java
+++ src/main/java/hicc_project/RottenToday/entity/Taste.java
@@ -2,15 +2,18 @@ package hicc_project.RottenToday.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @Entity
 public class Taste {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Appetite type;
-
     private String name;
     private String info;
     private String image;
@@ -25,9 +28,14 @@ public class Taste {
     @JoinColumn(name = "user_id")
     private User user;
 
-    public Taste(Long id, Appetite type, Recipe recipe){
+    @ManyToOne
+    @JoinColumn(name = "recipe_id")
+    private Recipe recipe;
+
+    public Taste(Long id, Appetite type, Recipe recipe) {
         this.id = id;
         this.type = type;
+        this.recipe = recipe; // 연관관계 설정
         this.name = recipe.getName();
         this.info = recipe.getInfo();
         this.image = recipe.getImage();
@@ -38,5 +46,4 @@ public class Taste {
         this.fat = recipe.getFat();
         this.portion = recipe.getPortion();
     }
-
 }
--- src/main/java/hicc_project/RottenToday/repository/CookHistoryRepository.java
+++ src/main/java/hicc_project/RottenToday/repository/CookHistoryRepository.java
@@ -1,6 +1,7 @@
 package hicc_project.RottenToday.repository;
 
+import hicc_project.RottenToday.entity.CookHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CookHistoryRepository extends JpaRepository<CookHistoryRepository, Long> {
+public interface CookHistoryRepository extends JpaRepository<CookHistory, Long> {
 }
