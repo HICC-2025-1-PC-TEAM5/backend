1a7f2a121a165852a71048be38c1e0c150a0210b
DESKTOP-100KUGP\준연 <whwnsdus11@naver.com>
2025-07-30 00:16:52 +0900

    fix: 오류수정 반영

--- src/main/java/hicc_project/RottenToday/repository/AllergyRepository.java
+++ src/main/java/hicc_project/RottenToday/repository/AllergyRepository.java
@@ -1,6 +1,7 @@
 package hicc_project.RottenToday.repository;
 
+import hicc_project.RottenToday.entity.Allergy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AllergyRepository extends JpaRepository<AllergyRepository, Long> {
+public interface AllergyRepository extends JpaRepository<Allergy, Long> {
 }
