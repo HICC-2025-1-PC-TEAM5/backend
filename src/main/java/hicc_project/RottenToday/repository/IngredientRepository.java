107516942c3c7af4ca2557e172d1c74af4bb9864
DESKTOP-100KUGP\준연 <whwnsdus11@naver.com>
2025-08-08 15:07:53 +0900

    feat: 냉장고 재료 관련 기능 구현

--- src/main/java/hicc_project/RottenToday/repository/IngredientRepository.java
+++ src/main/java/hicc_project/RottenToday/repository/IngredientRepository.java
@@ -2,9 +2,13 @@ package hicc_project.RottenToday.repository;
 
 import hicc_project.RottenToday.entity.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 
+    @Query("SELECT i FROM Ingredient i WHERE i.name = :name")
+    Optional<Ingredient> findByName(String name);
 }
--- src/main/java/hicc_project/RottenToday/repository/RefrigeratorIngredientRepository.java
+++ src/main/java/hicc_project/RottenToday/repository/RefrigeratorIngredientRepository.java
@@ -2,6 +2,12 @@ package hicc_project.RottenToday.repository;
 
 import hicc_project.RottenToday.entity.RefrigeratorIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RefrigeratorIngredientRepository extends JpaRepository<RefrigeratorIngredient, Long> {
+
+    @Query("SELECT r FROM RefrigeratorIngredient r WHERE r.member.id = :memberId")
+    List<RefrigeratorIngredient> findByMemberId(Long memberId);
 }
