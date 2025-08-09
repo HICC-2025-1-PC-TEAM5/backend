914ae6f771429145d3a3369467b2c7eebab6582f
DESKTOP-100KUGP\준연 <whwnsdus11@naver.com>
2025-08-09 18:19:45 +0900

    feat: 알러지 관련 기능 구현

---
+++ src/main/java/hicc_project/RottenToday/dto/AllergyResponse.java
@@ -0,0 +1,19 @@
+package hicc_project.RottenToday.dto;
+
+import hicc_project.RottenToday.entity.Allergy;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AllergyResponse {
+    private Long allergyId;
+    private String name;
+    private String category;
+
+    public AllergyResponse(Allergy allergy) {
+        this.allergyId = allergy.getId();
+        this.name = allergy.getIngredient().getName();
+        this.category = allergy.getIngredient().getCategory().getType();
+    }
+}
